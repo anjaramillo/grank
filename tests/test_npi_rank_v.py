5a7a33850dce0e80df5a8c934ca86f59970d8631
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2023-04-08 18:00:22 +1000

    add tests

---
+++ tests/test_npi_rank_v.py
@@ -0,0 +1,216 @@
+import numpy as np
+import pytest
+from numpy.testing import assert_array_equal, assert_equal
+
+from grank import npi_rank_v
+
+
+class TestRankData:
+    def test_empty(self):
+        """stats.npi_rank_v([]) should return an empty array."""
+        a = np.array([], dtype=int)
+        r = npi_rank_v(a)
+        assert_array_equal(r, np.array([], dtype=np.float64))
+        r = npi_rank_v([])
+        assert_array_equal(r, np.array([], dtype=np.float64))
+
+    def test_one(self):
+        """Check stats.npi_rank_v with an array of length 1."""
+        data = [100]
+        a = np.array(data, dtype=int)
+        r = npi_rank_v(a)
+        assert_array_equal(r, np.array([1.0], dtype=np.float64))
+        r = npi_rank_v(data)
+        assert_array_equal(r, np.array([1.0], dtype=np.float64))
+
+    def test_basic(self):
+        """Basic tests of stats.npi_rank_v."""
+        data = [100, 10, 50]
+        expected = np.array([3.0, 1.0, 2.0], dtype=np.float64)
+        a = np.array(data, dtype=int)
+        r = npi_rank_v(a)
+        assert_array_equal(r, expected)
+        r = npi_rank_v(data)
+        assert_array_equal(r, expected)
+
+        data = [40, 10, 30, 10, 50]
+        expected = np.array([4.0, 1.5, 3.0, 1.5, 5.0], dtype=np.float64)
+        a = np.array(data, dtype=int)
+        r = npi_rank_v(a)
+        assert_array_equal(r, expected)
+        r = npi_rank_v(data)
+        assert_array_equal(r, expected)
+
+        data = [20, 20, 20, 10, 10, 10]
+        expected = np.array([5.0, 5.0, 5.0, 2.0, 2.0, 2.0], dtype=np.float64)
+        a = np.array(data, dtype=int)
+        r = npi_rank_v(a)
+        assert_array_equal(r, expected)
+        r = npi_rank_v(data)
+        assert_array_equal(r, expected)
+        # The docstring states explicitly that the argument is flattened.
+        # a2d = a.reshape(2, 3)
+        # r = npi_rank_v(a2d)
+        # assert_array_equal(r, expected)
+
+    def test_rankdata_object_string(self):
+        min_rank = lambda a: [1 + sum(i < j for i in a) for j in a]
+        max_rank = lambda a: [sum(i <= j for i in a) for j in a]
+        ordinal_rank = lambda a: min_rank([(x, i) for i, x in enumerate(a)])
+
+        def average_rank(a):
+            return [(i + j) / 2.0 for i, j in zip(min_rank(a), max_rank(a))]
+
+        def dense_rank(a):
+            b = np.unique(a)
+            return [1 + sum(i < j for i in b) for j in a]
+
+        rankf = dict(
+            min=min_rank,
+            max=max_rank,
+            ordinal=ordinal_rank,
+            average=average_rank,
+            dense=dense_rank,
+        )
+
+        def check_ranks(a):
+            for method in "min", "max", "dense", "ordinal", "average":
+                out = npi_rank_v(a, method=method)
+                assert_array_equal(out, rankf[method](a))
+
+        val = ["foo", "bar", "qux", "xyz", "abc", "efg", "ace", "qwe", "qaz"]
+        check_ranks(np.random.choice(val, 200))
+        check_ranks(np.random.choice(val, 200).astype("object"))
+
+        val = np.array([0, 1, 2, 2.718, 3, 3.141], dtype="object")
+        check_ranks(np.random.choice(val, 200).astype("object"))
+
+    def test_large_int(self):
+        data = np.array([2**60, 2**60 + 1], dtype=np.uint64)
+        r = npi_rank_v(data)
+        assert_array_equal(r, [1.0, 2.0])
+
+        data = np.array([2**60, 2**60 + 1], dtype=np.int64)
+        r = npi_rank_v(data)
+        assert_array_equal(r, [1.0, 2.0])
+
+        data = np.array([2**60, -(2**60) + 1], dtype=np.int64)
+        r = npi_rank_v(data)
+        assert_array_equal(r, [2.0, 1.0])
+
+    def test_big_tie(self):
+        for n in [10000, 100000, 1000000]:
+            data = np.ones(n, dtype=int)
+            r = npi_rank_v(data)
+            expected_rank = 0.5 * (n + 1)
+            assert_array_equal(r, expected_rank * data, "test failed with n=%d" % n)
+
+    def test_axis(self):
+        data = [[0, 2, 1], [4, 2, 2]]
+        expected0 = [[1.0, 1.5, 1.0], [2.0, 1.5, 2.0]]
+        r0 = npi_rank_v(data, axis=0)
+        assert_array_equal(r0, expected0)
+        expected1 = [[1.0, 3.0, 2.0], [3.0, 1.5, 1.5]]
+        r1 = npi_rank_v(data, axis=1)
+        assert_array_equal(r1, expected1)
+
+    methods = ["average", "min", "max", "dense", "ordinal"]
+    dtypes = [np.float64] + [np.int_] * 4
+
+    @pytest.mark.parametrize("axis", [0, 1])
+    @pytest.mark.parametrize("method, dtype", zip(methods, dtypes))
+    def test_size_0_axis(self, axis, method, dtype):
+        shape = (3, 0)
+        data = np.zeros(shape)
+        r = npi_rank_v(data, method=method, axis=axis)
+        assert_equal(r.shape, shape)
+        assert_equal(r.dtype, dtype)
+
+
+_cases = (
+    # values, method, expected
+    ([], "average", []),
+    ([], "min", []),
+    ([], "max", []),
+    ([], "dense", []),
+    ([], "ordinal", []),
+    #
+    ([100], "average", [1.0]),
+    ([100], "min", [1.0]),
+    ([100], "max", [1.0]),
+    ([100], "dense", [1.0]),
+    ([100], "ordinal", [1.0]),
+    #
+    ([100, 100, 100], "average", [2.0, 2.0, 2.0]),
+    ([100, 100, 100], "min", [1.0, 1.0, 1.0]),
+    ([100, 100, 100], "max", [3.0, 3.0, 3.0]),
+    ([100, 100, 100], "dense", [1.0, 1.0, 1.0]),
+    ([100, 100, 100], "ordinal", [1.0, 2.0, 3.0]),
+    #
+    ([100, 300, 200], "average", [1.0, 3.0, 2.0]),
+    ([100, 300, 200], "min", [1.0, 3.0, 2.0]),
+    ([100, 300, 200], "max", [1.0, 3.0, 2.0]),
+    ([100, 300, 200], "dense", [1.0, 3.0, 2.0]),
+    ([100, 300, 200], "ordinal", [1.0, 3.0, 2.0]),
+    #
+    ([100, 200, 300, 200], "average", [1.0, 2.5, 4.0, 2.5]),
+    ([100, 200, 300, 200], "min", [1.0, 2.0, 4.0, 2.0]),
+    ([100, 200, 300, 200], "max", [1.0, 3.0, 4.0, 3.0]),
+    ([100, 200, 300, 200], "dense", [1.0, 2.0, 3.0, 2.0]),
+    ([100, 200, 300, 200], "ordinal", [1.0, 2.0, 4.0, 3.0]),
+    #
+    ([100, 200, 300, 200, 100], "average", [1.5, 3.5, 5.0, 3.5, 1.5]),
+    ([100, 200, 300, 200, 100], "min", [1.0, 3.0, 5.0, 3.0, 1.0]),
+    ([100, 200, 300, 200, 100], "max", [2.0, 4.0, 5.0, 4.0, 2.0]),
+    ([100, 200, 300, 200, 100], "dense", [1.0, 2.0, 3.0, 2.0, 1.0]),
+    ([100, 200, 300, 200, 100], "ordinal", [1.0, 3.0, 5.0, 4.0, 2.0]),
+    #
+    ([10] * 30, "ordinal", np.arange(1.0, 31.0)),
+)
+
+
+def test_cases():
+    for values, method, expected in _cases:
+        group = np.tile(np.arange(3), len(values))
+        values = np.repeat(values, 3)
+        expected = np.repeat(expected, 3)
+        r = npi_rank_v(values, group, method=method)
+        assert_array_equal(r, expected)
+
+
+def test_manual():
+    gid = [0, 1, 0, 1, 0, 1]
+    val = [1, 3, 6, 8, 4, 3]
+    ranks = npi_rank_v(val, gid, method="average")
+    assert_array_equal(ranks, [1, 1.5, 3, 3, 2, 1.5])
+
+    ranks = npi_rank_v(val, gid, method="min")
+    assert_array_equal(ranks, [1, 1, 3, 3, 2, 1])
+
+    ranks = npi_rank_v(val, gid, method="max")
+    assert_array_equal(ranks, [1, 2, 3, 3, 2, 2])
+
+    ranks = npi_rank_v(val, gid, method="ordinal")
+    assert_array_equal(ranks, [1, 1, 3, 3, 2, 2])
+
+    ranks = npi_rank_v(val, gid, method="dense")
+    assert_array_equal(ranks, [1, 1, 3, 2, 2, 1])
+
+
+def test_manual2():
+    gid = [0, 1, 0, 0, 1, 0, 1, 2, 2, 1]
+    val = [1, 6, 6, 4, 6, 4, 7, 7, 7, 6]
+    ranks = npi_rank_v(val, gid, method="average")
+    assert_array_equal(ranks, [1, 2, 4, 2.5, 2, 2.5, 4, 1.5, 1.5, 2])
+
+    ranks = npi_rank_v(val, gid, method="min")
+    assert_array_equal(ranks, [1, 1, 4, 2, 1, 2, 4, 1, 1, 1])
+
+    ranks = npi_rank_v(val, gid, method="max")
+    assert_array_equal(ranks, [1, 3, 4, 3, 3, 3, 4, 2, 2, 3])
+
+    ranks = npi_rank_v(val, gid, method="ordinal")
+    assert_array_equal(ranks, [1, 1, 4, 2, 2, 3, 4, 1, 2, 3])
+
+    ranks = npi_rank_v(val, gid, method="dense")
+    assert_array_equal(ranks, [1, 1, 3, 2, 1, 2, 2, 1, 1, 1])
---
+++ tests/test_performance.py
@@ -0,0 +1,92 @@
+import time
+from typing import Any, Dict
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from numpy.testing import assert_array_equal
+
+from grank import grank, npi_rank_n, npi_rank_v
+
+
+def run_once(groups: int, group_size: int, method: str) -> pd.DataFrame:
+    a = np.random.randint(0, group_size, group_size * groups)
+    g = np.random.randint(0, groups, group_size * groups)
+
+    t1 = time.time()
+    result_a = grank(a, g, method)
+    t2 = time.time()
+    time_grank = t2 - t1
+    print(f"grank: {t2-t1}")
+
+    t1 = time.time()
+    result_b = npi_rank_n(a, g, method)
+    t2 = time.time()
+    time_nprank = t2 - t1
+    print(f"npi_rank_n: {t2-t1}")
+
+    t1 = time.time()
+    result_c = npi_rank_v(a, g, method)
+    t2 = time.time()
+    time_npirank = t2 - t1
+    print(f"npi_rank_v: {t2-t1}")
+
+    df = pd.DataFrame({"a": a, "g": g})
+    method = method if method != "ordinal" else "first"
+    t1 = time.time()
+    result_d = df.groupby("g")["a"].rank(method)
+    t2 = time.time()
+    time_pandas = t2 - t1
+    print(f"pandas: {t2-t1}")
+
+    assert_array_equal(result_a, result_b)  # type: ignore
+    assert_array_equal(result_a, result_c)  # type: ignore
+    assert_array_equal(result_a, result_d)  # type: ignore
+
+    return {
+        "grank": time_grank,
+        "npi_rank_n": time_nprank,
+        "npi_rank_v": time_npirank,
+        "pandas.rank": time_pandas,
+    }
+
+
+def main() -> None:
+    np.random.seed(0)
+    group_size = 100
+    for method in ["average", "min", "max", "dense", "ordinal"]:
+        data = []
+        for groups in [10, 100, 1000, 10000, 100000, 1000000]:
+            result: Dict[str, Any] = {}
+            result["groups"] = groups
+            print(f"Groups: {groups}")
+            result.update(run_once(groups, group_size, method))
+            data.append(result)
+        df = pd.DataFrame(data)
+        print(
+            df.to_markdown(
+                f"assets/{method}.md", index=False, floatfmt=[".0f"] * 1 + [".3f"] * 4
+            )
+        )
+        df = df.melt(
+            id_vars=["groups"],
+            value_vars=["grank", "npi_rank_n", "npi_rank_v", "pandas.rank"],
+            var_name="function",
+            value_name="seconds",
+        )
+        sns.set_theme("notebook", "darkgrid", font="Linux Biolinum O")
+        g = sns.relplot(
+            kind="line",
+            data=df,
+            x="groups",
+            y="seconds",
+            hue="function",
+            aspect=2,
+        )
+        g.set(xscale="log", yscale="log")
+        g.tight_layout()
+        g.savefig(f"assets/{method}.png", dpi=600)
+
+
+if __name__ == "__main__":
+    main()
